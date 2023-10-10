19731c3bba59933ca7a7c8b015baa3542db2f404
caiselvass <caiselvassala@gmail.com>
2023-10-10 23:24:30 +0200

    multiple files

--- problem_bicing.py
+++ problem_bicing.py
@@ -1,11 +1,11 @@
 from aima.search import Problem, hill_climbing, simulated_annealing
+from state_bicing import EstadoBicing
 
 class ProblemaBicing(Problem):
     def __init__(self, initial_state: EstadoBicing):
         self.expanded_nodes = 0
         super.__init__(initial_state)
         pass
-
 
     def actions(self, state: EstadoBicing):
         pass
@@ -18,3 +18,9 @@ class ProblemaBicing(Problem):
     
     def h(self, node):
         return node.state.heuristic()
+    
+    def value(self, state: EstadoBicing):
+        return -state.heuristic()
+    
+    def goal_test(self, state: EstadoBicing):
+        return False
