e3c1b32e07bdf398f038bdbe7b2325ee003fd413
rogerbaiges <rogerbaigestrilla@gmail.com>
2023-10-10 11:45:56 +0200

    cai gei

--- abia_bicing.py
+++ abia_bicing.py
@@ -249,3 +249,5 @@ if __name__ == '__main__':
 
     print("Bicis= %3d Demanda= %3d Disponibles= %3d Necesitan= %3d" %
           (acum_bicicletas, acum_demanda, acum_disponibles, acum_necesarias))
+
+# BAIGES
